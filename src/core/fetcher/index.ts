196cb42dc04d4d1b2ea7f6031d57f5427b0695d4
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-02-07 22:36:36 -0300

    Add binder

--- src/core/fetcher/index.ts
+++ src/core/fetcher/index.ts
@@ -1,1 +1,18 @@
-// NOOP (TODO)
+import { gitHubAPIFetcher } from '../../services/github/fetcher';
+import * as Fetcher from './fetcher';
+
+const commonDeps = {
+  gitHubAPIFetcher
+};
+
+export const lookupUserDataFetcher = Fetcher.createLookupUserDataFetcher(
+  commonDeps
+);
+
+export const lookupUserFollowerListFetcher = Fetcher.createLookupUserFollowerListFetcher(
+  commonDeps
+);
+
+export const lookupUserFollowingListFetcher = Fetcher.createLookupUserFollowingListFetcher(
+  commonDeps
+);
